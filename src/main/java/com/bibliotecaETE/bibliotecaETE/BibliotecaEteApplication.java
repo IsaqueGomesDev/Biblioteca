4fed8ffebc72017ec87014adbdb9d5a5e84c1372
SasukeCigano <silvamariasilva301@gmail.com>
2026-02-05 14:03:58 -0300

    Ajuste

---
+++ src/main/java/com/bibliotecaETE/bibliotecaETE/BibliotecaEteApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotecaETE.bibliotecaETE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaEteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibliotecaETE/bibliotecaETE/BibliotecaEteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecaETE.bibliotecaETE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaEteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
